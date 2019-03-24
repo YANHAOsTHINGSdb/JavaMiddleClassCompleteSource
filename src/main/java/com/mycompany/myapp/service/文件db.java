7610a84f829726f1479047a24a980ee9bcd0894f
YANHAOsTHINGSdb <47000444+YANHAOsTHINGSdb@users.noreply.github.com>
2019-03-24 14:15:25 +0900

    再改一次

--- src/main/java/com/mycompany/myapp/service/文件db.java
+++ src/main/java/com/mycompany/myapp/service/文件db.java
@@ -20,7 +20,7 @@ import lombok.Data;
 @Data
 public class 文件db {
 
-	String sPath = "C:\\tmp\\";  // for windows
+	String sPath = "C:\\tmp";  // for windows
 	// String sPath = "/Users/haoyan/Desktop/data";  // for mac
 	// String sPath = "";
 	String sSubFolder = null;
